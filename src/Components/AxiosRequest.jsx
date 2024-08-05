ebbf2c44ca21933d5e874e54b0eb6b68c2852cb6
ali-ranah <ranasaimali1234@gmail.com>
2024-08-05 06:41:52 +0500

    New Changes

--- src/Components/AxiosRequest.jsx
+++ src/Components/AxiosRequest.jsx
@@ -3,8 +3,7 @@
 import axios from 'axios';
 
 const AxiosRequest = axios.create({
-  baseURL: 'http://localhost:5002'
+  baseURL: 'https://yazan-s1ux.onrender.com'
 });
-
-// https://yazan-s1ux.onrender.com
+ 
 export default AxiosRequest;
